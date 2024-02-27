5694378378ff4b4415057e2aae0b349db0e23a3d
Mohamed Nabil Hashem <mohamedn888@yahoo.com>
2024-02-27 09:41:38 +0200

    What finished

--- Airline_Reservation_System/main.c
+++ Airline_Reservation_System/main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+#define MAX_FLIGHTS 100
+#define MAX_PASSENGERS 100
+
+int flights[MAX_FLIGHTS][3]; //flight number, no of seats, flight date
+
+int passenger[MAX_PASSENGERS][4]; //ID, Name, Address, Phone number
+
 void admin_Settings();
 
 void passenger_Settings();
 
+void loginSignup();
 
 int main()
 {
@@ -100,5 +108,12 @@ void passenger_Settings(){
         printf("Invalid Input!");
         goto invalid;
     }
+
+}
+
+void loginSignup(){
+    unsigned int ID;
+    printf("Enter your ID: ");
+    scanf("%d", &ID);
 
 }
