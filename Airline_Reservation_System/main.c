c8958dff55a23aba0c1e3082a2748bcac05f97d5
Mohamed Nabil Hashem <mohamedn888@yahoo.com>
2024-03-01 19:50:25 +0200

    adding header file

--- Airline_Reservation_System/main.c
+++ Airline_Reservation_System/main.c
@@ -1,62 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-
-#define MAX_FLIGHTS 100
-#define MAX_PASSENGERS 100
-
-// System data structure
-struct flight{
-    int flightNumber;
-    int availableSeats;
-    char flightDate[20]; // DD/MM/YYYY
-    char departureTime[20]; // HH:MM
-    char departureCity[50];
-    char arrivalTime[20]; // HH:MM
-    char arrivalCity[50];
-
-};
-
-struct passenger{
-    int ID;
-    char Name[20];
-    char Address[100];
-    int phoneNumber;
-
-};
-
-struct ticket{
-    int number;
-    int passengerID;
-    int flightNumber;
-    char resDate[20]; // DD/MM/YYYY
-    ///??????????????????????????
-    char departure_city[20];
-    char departure_time[20];
-    char arrival_city[20];
-    char arrival_time[20];
-
-};
-
-// Global Variables, Arrays & Objects
-struct flight trip[MAX_FLIGHTS];
-struct passenger pasngr[MAX_PASSENGERS];
-struct ticket tkt[MAX_PASSENGERS];
-int flightsExist = 0;
-int passengersExist = 0;
-int TicketExist = 0;
-
-
-
-
-//Prototype Functions
-void admin_Settings();
-void passenger_Settings();
-int loginSignup();
-int New_Flight_Schedule(void);
-void update_flight();
-int New_Passenger_Resservation(void);
-void modify_reservation();
+#include "projectHeader.h"
 
 //Testing Area
 
---
+++ Airline_Reservation_System/projectHeader.h
@@ -0,0 +1,53 @@
+#ifndef _PROJECTHEADER.H _
+#define _PROJECTHEADER.H _
+#define MAX_FLIGHTS 100
+#define MAX_PASSENGERS 100
+
+// System data structure
+struct flight{
+    int flightNumber;
+    int availableSeats;
+    char flightDate[20]; // DD/MM/YYYY
+    char departureTime[20]; // HH:MM
+    char departureCity[50];
+    char arrivalTime[20]; // HH:MM
+    char arrivalCity[50];
+
+};
+
+struct passenger{
+    int ID;
+    char Name[20];
+    char Address[100];
+    int phoneNumber;
+
+};
+
+struct ticket{
+    int number;
+    int passengerID;
+    int flightNumber;
+    char resDate[20]; // DD/MM/YYYY
+
+};
+
+// Global Variables, Arrays & Objects
+struct flight trip[MAX_FLIGHTS];
+struct passenger pasngr[MAX_PASSENGERS];
+struct ticket tkt[MAX_PASSENGERS];
+int flightsExist = 0;
+int passengersExist = 0;
+int TicketExist = 0;
+
+
+
+
+//Prototype Functions
+void admin_Settings();
+void passenger_Settings();
+int loginSignup();
+int New_Flight_Schedule(void);
+void update_flight();
+int New_Passenger_Resservation(void);
+void modify_reservation();
+#endif
